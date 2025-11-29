be8282a1b35a8237219634933c977b8bb83d43a7
soumya-sunkad <soumyasunkad06@gmail.com>
2025-11-29 11:13:58 +0530

    save

---
+++ file1.py
@@ -0,0 +1,18 @@
+import sys
+if(sys.argv)!=3:
+   print("error:Python file1.py number,name,age")
+   sys.exit(1)
+   num=int(sys.argv[1])
+   name=int(sys.argv[2])
+   age=int(sys.argv[3])
+   print("Train Ticket Reservation:")
+   print("Train Number:",num)
+   print("Name of the passenger:",name)
+   print("Age of passenger:",age)
+else:
+    num="101"
+    name="John"
+    age=30
+    print("Train Number:",101)
+    print("Name of the passenger:","John")
+    print("Age of passenger:",30)
